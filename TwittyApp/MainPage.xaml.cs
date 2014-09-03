117507718e8920be0ad22fbfac5098515a7e7d28
Goi Jia Jian <icesurvivr1337@hotmail.com>
2014-09-03 17:50:26 +0800

    stuff

---
+++ TwittyApp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using TwittyApp.Resources;
+using Microsoft.Phone.Net.NetworkInformation;
+using TweetSharp;
+
+namespace TwittyApp {
+	public partial class MainPage : PhoneApplicationPage {
+
+		private string apiKey = "tr4zL07a1kbMFSYapIrYJ3QZ9";
+		private string apiKeySecret = "l77XyL4Wf2XuquLngEInQpIOi1Tv1eOoPBHJfbDdZhwIoO7GP3";
+
+		private string accessToken = "2733902508-dUZ0PntOXx3WqvNFy3e1Yk3UaU7vXTsqvxowjlb";
+		private string accessTokenSecret = "JtOvSMZktJhVEpGutlEXC6QjB2qo9Dq467DN1QtNCSnor";
+
+		// Constructor
+		public MainPage() {
+			InitializeComponent();
+		}
+
+		private void btnSearch_Click(object sender, RoutedEventArgs e) {
+			string twitterHandle = tbxTwitterHandle.Text;
+
+			if (DeviceNetworkInformation.IsNetworkAvailable) {
+				var service = new TwitterService(apiKey, apiKeySecret);
+				service.AuthenticateWith(accessToken, accessTokenSecret);
+				llsSearchTwitterResults.ItemsSource = null;
+
+				// Use ListTweetsOnUserTimeline to get all tweets from user. From the TweetSharp lib.
+				service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions() {
+					ScreenName = twitterHandle
+				}, (tweets, response) => {
+
+					if (response.StatusCode == HttpStatusCode.OK) {
+						this.Dispatcher.BeginInvoke(() => {
+							tblkHeader.Text = twitterHandle;
+							llsSearchTwitterResults.ItemsSource = tweets.ToList();
+						});
+					}
+				});
+			} else {
+
+			}
+		}
+	}
+}
