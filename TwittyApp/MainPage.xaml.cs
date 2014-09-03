f72189a68af5df10f37b19ea8e7f7abd844b5404
Goi Jia Jian <icesurvivr1337@hotmail.com>
2014-09-03 23:32:04 +0800

    really the final one

--- TwittyApp/MainPage.xaml.cs
+++ TwittyApp/MainPage.xaml.cs
@@ -29,29 +29,25 @@ namespace TwittyApp {
 			string twitterHandle = tbxTwitterHandle.Text;
 
 			if (DeviceNetworkInformation.IsNetworkAvailable) {
-				try {
-					var service = new TwitterService(apiKey, apiKeySecret);
-					service.AuthenticateWith(accessToken, accessTokenSecret);
-					llsSearchTwitterResults.ItemsSource = null;
+				var service = new TwitterService(apiKey, apiKeySecret);
+				service.AuthenticateWith(accessToken, accessTokenSecret);
+				llsSearchTwitterResults.ItemsSource = null;
 
-					System.Collections.Generic.IEnumerable<TweetSharp.TwitterStatus> tweetsTest = null;
-					service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions() {
-						ScreenName = twitterHandle
-					}, (tweets, response) => {
+				System.Collections.Generic.IEnumerable<TweetSharp.TwitterStatus> tweetsTest = null;
+				service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions() {
+					ScreenName = twitterHandle
+				}, (tweets, response) => {
 
-						if (response.StatusCode == HttpStatusCode.OK) {
-							this.Dispatcher.BeginInvoke(() => {
-								tblkHeader.Text = twitterHandle;
-								llsSearchTwitterResults.ItemsSource = tweets.ToList();
-								tweetsTest = tweets;
-							});
-						}
-					});
-				} catch (Exception ex) {
-					MessageBox.Show("Oops! Can't fetch this user's tweets! Ex: " + ex.ToString());
-				}
+					if (response.StatusCode == HttpStatusCode.OK) {
+						this.Dispatcher.BeginInvoke(() => {
+							tblkHeader.Text = twitterHandle;
+							llsSearchTwitterResults.ItemsSource = tweets.ToList();
+							tweetsTest = tweets;
+						});
+					}
+				});
 			} else {
-
+				MessageBox.Show("No network connectivity! :(");
 			}
 		}
 	}
